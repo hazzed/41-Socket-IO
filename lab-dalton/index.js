3e0595436b081fd309ea11c7a40963e2802acd0d
Dalton Carr <daltonlcarr@yahoo.com>
2018-01-29 19:19:34 -0800

    finally got username working

--- lab-dalton/index.js
+++ lab-dalton/index.js
@@ -5,16 +5,40 @@ let io = require('socket.io')(http);
 
 app.use(express.static('./public'));
 
+const USERS = {};
+
+
 io.on('connection', (socket) => {
   console.log('Joined', socket.id);
+  USERS[socket.id] = {};
+  USERS[socket.id].username = 'to the chat!';
+
+  console.log(USERS[socket.id].username);
+  socket.emit('set-header', {username: USERS[socket.id].username});
+
+  io.emit('userData', USERS);
+
+  console.log('Joined', USERS[socket.id].username);
 
   socket.on('disconnect', () => {
     console.log('Left', socket.id);
+    delete USERS[socket.id];
   });
 
   socket.on('send-message', (data) => {
+    data.username = USERS[socket.id].username;
     console.log('Message:', data.message);
     io.emit('receive-message', data);
+  });
+
+  socket.on('submit-username', (msg) =>{
+    console.log(msg.username);
+    if(msg.username === '')
+      return;
+    console.log(USERS);
+    USERS[socket.id].username = msg.username;
+    io.emit('userData', USERS);
+    console.log(USERS);
   });
 });
 
--- lab-dalton/public/js/main.js
+++ lab-dalton/public/js/main.js
@@ -2,20 +2,44 @@ const socket = io();
 
 console.log('ID', socket.id);
 
-let sendMessageForm = document.getElementById('send-message-form');
-let messageInput = document.getElementById('message-input');
-let messagesContainer = document.getElementById('messages');
+const sendMessageForm = document.getElementById('send-message-form');
+const messageInput = document.getElementById('message-input');
+const messagesContainer = document.getElementById('messages');
+const usernameForm = document.getElementById('username-form');
 
-sendMessageForm.addEventListener('submit', (ev) => {
-  ev.preventDefault();
+usernameForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  
+  const usernameEl = document.getElementById('username-input');
+  const username = usernameEl.value;
+  if(username === '') {
+    return;
+  } else {
+    socket.emit('submit-username', {username});
+    let header = document.getElementById('yoyo');
+    header.textContent = 'Welcome ' + username.toUpperCase() + '!';
+    
+  }
+});
+
+sendMessageForm.addEventListener('submit', (event) => {
+  event.preventDefault();
   let message = messageInput.value;
   socket.emit('send-message', {message: message});
+  messageInput.value = '';
 });
 
 socket.on('receive-message', (data) => {
   console.log('RECEIVED:', data);
+  let timestamp = new Date().toLocaleTimeString();
   let div = document.createElement('div');
-  div.innerHTML = data.message;
+  let codename = data.username.toUpperCase();
+  div.textContent = timestamp + '' + codename + '' + data.message;
   messagesContainer.appendChild(div);
 });
 
+socket.on('set-header', (data) => {
+  let codename = data.username.toUpperCase();
+  let header = document.getElementById('yoyo');
+  header.append( 'WELCOME ' + codename);
+});
